2c2cc8b6f35a911c247bf697977ec6f2eec2e370
andreafoti <andreafoti2014@gmail.com>
2020-08-08 13:02:27 -0300

    Login
    Added login

--- js/login.js
+++ js/login.js
@@ -2,5 +2,16 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    
 });
+
+function onSubmit(event){
+    event.preventDefault()
+    const email = document.querySelector("#correo").nodeValue;
+    const password =document.querySelector("#password").nodeValue;
+    if(email != "" && password !=""){
+        window.location = "index.html";
+        sessionStorage.setItem('isLogged', 'true');
+    }
+    
+}
